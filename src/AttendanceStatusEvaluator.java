a31d1120909a5065e659608f07573b51e2fac9dd
Naveen Kumar <KUMAR2004199@GMAIL.COM>
2026-01-19 16:15:30 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,25 @@
 
+import java.util.Scanner;
+
+public class AttendanceStatusEvaluator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int Attendance = sc.nextInt();
+        if (0 <= Attendance){
+            if (Attendance < 60){
+                System.out.println("Poor");
+            }
+            else if ((Attendance >= 60) && (Attendance <=85)){
+                System.out.println("Satisfactory");
+            }
+            else if (Attendance >85){
+                System.out.println("Excellene");
+            }
+        }
+
+        sc.close();
+    }
+}
