003871e037b0ebd92dd48dd63e577967e0489d03
Naveen Kumar <KUMAR2004199@GMAIL.COM>
2026-01-19 20:44:40 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -16,7 +16,7 @@ public class AttendanceStatusEvaluator {
                 System.out.println("Satisfactory");
             }
             else if (Attendance >85){
-                System.out.println("Excellenet");
+                System.out.println("Excellent");
             }
         }
 
